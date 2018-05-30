9f93f169abe9cca7ab7e99c0312756b48750cd08
Effluxation <patrick.dorosz@gmail.com>
2018-05-30 15:34:29 -0400

    Latest build

---
+++ README.md
@@ -0,0 +1,1 @@
+My portfolio site created as part of Udacity's Front-end Nanodegree program.
---
+++ scripts/main.min.06f67f3d.js
@@ -0,0 +1,3 @@
+/*! projects--portfolio | Patrick Dorosz | ISC */
+
+!function(){"use strict";var t=document.querySelector(".entypo-mail");t.onclick=function(){t.href="mailto:"+t.dataset.lhs_a+"."+t.dataset.lhs_b+"@"+t.dataset.dom+"."+t.dataset.tld}}();
